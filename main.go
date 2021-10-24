c1fafc701b8d39393e523cddf5895cdc18fc2620
Pat Hacker <path@cleanmarklabels.com>
2021-10-24 12:10:51 -0700

    adding http.Handle & HandleFunc into the mix

--- main.go
+++ main.go
@@ -9,9 +9,19 @@ import (
 type Wstr string
 
 func (s Wstr) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, s)
+	fmt.Fprintf(w, "Hello from barFunc: %s", s)
 }
 
+// If ListenAndServe has nil as the Handler, then it uses http.DefaultServeMux.
+// http.Handle and http.HandleFunc register their Handlers with http.DefaultServeMux.
+
 func main() {
-	log.Fatal(http.ListenAndServe(":8080", Wstr("Hello world!")))
+	// note that localhost:8080/bar goes here
+	http.Handle("/bar", Wstr("barbarbar"))
+
+	//  while everything else goes here.
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "Hello World!, from the HandleFunc")
+	})
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
