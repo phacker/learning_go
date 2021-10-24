bb8a8312f73a1e97f88bcd5122452ea810421113
Pat Hacker <path@cleanmarklabels.com>
2021-10-24 16:29:07 -0700

    simple logging middleware

--- main.go
+++ main.go
@@ -6,6 +6,13 @@ import (
 	"net/http"
 )
 
+func logging(f http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		log.Println(r.URL.Path)
+		f(w, r)
+	}
+}
+
 func hello(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "hello")
 }
@@ -16,7 +23,7 @@ func world(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	m := http.NewServeMux()
-	m.HandleFunc("/hello/", hello)
-	m.HandleFunc("/world/", world)
+	m.HandleFunc("/hello/", logging(hello))
+	m.HandleFunc("/world/", logging(world))
 	log.Fatal(http.ListenAndServe(":8080", m))
 }
